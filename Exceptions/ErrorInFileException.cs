1856aa19c0de904a34978e8ca6de22c67b53fabb
dhfgf <55654624+dhfgf@users.noreply.github.com>
2020-11-12 15:33:50 +0300

    Delete ErrorInFileException.cs

--- Exceptions/ErrorInFileException.cs
+++
@@ -1,8 +0,0 @@
-namespace Lab1.Exceptions
-{
-    public class ErrorInFileException : MainException
-    {
-        public ErrorInFileException()
-            : base($"Something wrong in file") { }
-    }
-}
