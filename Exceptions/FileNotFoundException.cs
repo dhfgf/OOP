4780d0dd051c8bfeda079e8cab8f357610eddec1
dhfgf <55654624+dhfgf@users.noreply.github.com>
2020-11-12 15:34:07 +0300

    Delete FileNotFoundException.cs

--- Exceptions/FileNotFoundException.cs
+++
@@ -1,8 +0,0 @@
-namespace Lab1.Exceptions
-{
-    public class FileNotFoundException : MainException
-    {
-        public FileNotFoundException(string path)
-            : base($"File with path {path} not found") { }
-    }
-}
