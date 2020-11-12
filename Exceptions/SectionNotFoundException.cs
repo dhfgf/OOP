173a2ab9de146fa19b2e8b84f727a369e9b5a6b4
dhfgf <55654624+dhfgf@users.noreply.github.com>
2020-11-12 15:34:26 +0300

    Delete SectionNotFoundException.cs

--- Exceptions/SectionNotFoundException.cs
+++
@@ -1,8 +0,0 @@
-namespace Lab1.Exceptions
-{
-    public class SectionNotFoundException : MainException
-    {
-        public SectionNotFoundException(string section)
-            : base($"Section {section} not found") { }
-    }
-}
