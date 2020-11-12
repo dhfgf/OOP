830d29d49c59c063bb49c83696b732b7a4ccc4dc
dhfgf <55654624+dhfgf@users.noreply.github.com>
2020-11-12 15:34:42 +0300

    Delete WrongVarTypeException.cs

--- Exceptions/WrongVarTypeException.cs
+++
@@ -1,8 +0,0 @@
-namespace Lab1.Exceptions
-{
-    public class WrongVarTypeException : MainException
-    {
-        public WrongVarTypeException(string type)
-            : base($"This value is not {type}") { }
-    }
-}
