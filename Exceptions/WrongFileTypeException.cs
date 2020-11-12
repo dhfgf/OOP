960ab760ca816c286d12f9c52800eb2ab2965b74
dhfgf <55654624+dhfgf@users.noreply.github.com>
2020-11-12 15:34:35 +0300

    Delete WrongFileTypeException.cs

--- Exceptions/WrongFileTypeException.cs
+++
@@ -1,8 +0,0 @@
-namespace Lab1.Exceptions
-{
-    public class WrongFileTypeException : MainException
-    {
-        public WrongFileTypeException()
-            : base("File type is not .ini") { }
-    }
-}
