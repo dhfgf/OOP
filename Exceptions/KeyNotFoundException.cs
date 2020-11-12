2b3847e5285aa435f109f9db6d6d89490a2d6487
dhfgf <55654624+dhfgf@users.noreply.github.com>
2020-11-12 15:33:59 +0300

    Delete KeyNotFoundException.cs

--- Exceptions/KeyNotFoundException.cs
+++
@@ -1,8 +0,0 @@
-namespace Lab1.Exceptions
-{
-    public class KeyNotFoundException : MainException
-    {
-        public KeyNotFoundException(string key)
-            : base($"Key {key} not found") { }
-    }
-}
